7212ca675fea4d6fb29b0f683e53081848859ba2
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-02-19 12:00:55 +0700

    update form chatadmin

--- src/loading.jsx
+++ src/loading.jsx
@@ -3,7 +3,11 @@ import { Spinner } from "@nextui-org/react";
 export default function Loading() {
   return (
     <div className="flex gap-4 w-full h-screen items-center justify-center bg-black">
-      <img alt="logo" src="/logo.webp" className="w-[100px] h-[100px]" />
+      <img
+        alt="logo"
+        src="https://files.kampunginggrislc.com/2022/06/logo-lc-png-white.png"
+        className="w-[100px]"
+      />
       <Spinner color="white" />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </NextUIProvider>
   </React.StrictMode>
 );
